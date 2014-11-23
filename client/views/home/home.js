0183ec35f548efbde76cfb3b6401e02406419a71
Victor Leung <victorleungtw@gmail.com>
2014-11-23 23:22:58 +0800

    added profile filter for three tags

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -5,10 +5,22 @@ Template.Home.helpers({
     var selector = {};
 
     if (tag){
-      selector = {firstTag: tag};
-    }
+      var firstTagMatch = Profiles.find({firstTag: tag}).count();
+      var secondTagMatch = Profiles.find({secondTag: tag}).count();
+      var thirdTagMatch = Profiles.find({thirdTag: tag}).count();
 
-    console.log(selector);
+      if (firstTagMatch){
+        selector = {firstTag: tag};
+      }
+
+      if (secondTagMatch){
+        selector = {secondTag: tag};
+      }
+
+      if (thirdTagMatch){
+        selector = {thirdTag: tag};
+      }
+    }
 
     return Profiles.find(selector, {sort: {createdAt: -1}} );
   }
