3319b8256b9ff9782384f1dff4c2117d9c0ad953
Victor Leung <victorleungtw@Victors-MacBook-Air.local>
2015-03-18 19:24:17 +0800

    try jQuery file upload

--- client/index.js
+++ client/index.js
@@ -1,4 +1,15 @@
 Meteor.startup(function () {
+
+  Meteor.startup(function() {
+    Uploader.finished = function(index, fileInfo, templateContext) {
+      var userId = Meteor.userId();
+      var imagesURL = {
+        "profile.image": fileInfo.url
+      };
+      Meteor.users.update(userId, {$set: imagesURL});
+    };
+  });
+
   AccountsEntry.config({
     homeRoute: '/',
     dashboardRoute: '/profile',
